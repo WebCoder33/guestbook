8e1e021f1a8a3c27ddd7e88a821c2e4d8c6e25dc
WebCoder33 <coder33@yandex.ru>
2020-05-17 19:33:38 +0300

    add paginator

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,27 +5,45 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\ConferenceRepository;
 use Twig\Environment;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+
 
 class ConferenceController extends AbstractController
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
-    {
-    	$greet = '';
-    	if ($name = $request->query->get('hello')) {
-    		$greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-		}
-        /*return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);*/
+	/**
+	 * @Route("/", name="homepage")
+	 */
+	public function index(Environment $twig, ConferenceRepository $conferenceRepository)
+	{
 
-        return new Response($twig->render('conference/index.html.twig', [
-        	'conferences' => $conferenceRepository->findAll(),
+		return new Response($twig->render('conference/index.html.twig', [
+			'conferences' => $conferenceRepository->findAll(),
 		]));
-        
-    }
+
+	}
+
+
+	/**
+	 * @Route("/conference/{id}", name="conference")
+	 */
+	public function show(Request $request, Environment $twig, Conference
+	$conference, CommentRepository $commentRepository)
+	{
+		$offset = max(0, $request->query->getInt('offset', 0));
+		$paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+		return new Response($twig->render('conference/show.html.twig', [
+
+			'conference' => $conference,
+			'comments' => $paginator,
+			'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+			'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+
+		]));
+
+	}
 }
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use App\Entity\Comment;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Conference;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * @method Comment|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,37 +16,52 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Comment::class);
-    }
+	public const PAGINATOR_PER_PAGE = 2;
 
-    // /**
-    //  * @return Comment[] Returns an array of Comment objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+	public function __construct(ManagerRegistry $registry)
+	{
+		parent::__construct($registry, Comment::class);
+	}
 
-    /*
-    public function findOneBySomeField($value): ?Comment
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+	public function getCommentPaginator(Conference $conference, int $offset): Paginator
+	{
+		$query = $this->createQueryBuilder('c')
+		->andWhere('c.conference = :conference')
+		->setParameter('conference', $conference)
+		->orderBy('c.createdAt', 'DESC')
+		->setMaxResults(self::PAGINATOR_PER_PAGE)
+		->setFirstResult($offset)
+		->getQuery();
+
+		return new Paginator($query);
+	}
+
+// /**
+//  * @return Comment[] Returns an array of Comment objects
+//  */
+/*
+public function findByExampleField($value)
+{
+	return $this->createQueryBuilder('c')
+		->andWhere('c.exampleField = :val')
+		->setParameter('val', $value)
+		->orderBy('c.id', 'ASC')
+		->setMaxResults(10)
+		->getQuery()
+		->getResult()
+	;
+}
+*/
+
+/*
+public function findOneBySomeField($value): ?Comment
+{
+	return $this->createQueryBuilder('c')
+		->andWhere('c.exampleField = :val')
+		->setParameter('val', $value)
+		->getQuery()
+		->getOneOrNullResult()
+	;
+}
+*/
 }
