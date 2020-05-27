f02e84e9a93991a1957c66ee234f94f3570e1967
WebCoder33 <coder33@yandex.ru>
2020-05-27 16:54:05 +0300

    небольшой рефакторинг

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -29,7 +29,7 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertPageTitleContains('Amsterdam');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
-        $this->assertSelectorExists('div:contains("There are 2 comments.")');
+        $this->assertSelectorExists('div:contains("There are 1 comments.")');
     }
 
     public function testCommentSubmission()
@@ -47,7 +47,7 @@ class ConferenceControllerTest extends WebTestCase
         $comment->setState('published');
         self::$container->get(EntityManagerInterface::class)->flush();
         $client->followRedirect();
-        $this->assertSelectorExists('div:contains("There are 3 comments.")');
+        $this->assertSelectorExists('div:contains("There are 2 comments.")');
     }
 
 }
